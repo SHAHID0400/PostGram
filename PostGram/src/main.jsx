e216c7e9259cae2fe70221a7068567191c48b619
MD SHAHID <shahid.pp23001@gmail.com>
2026-05-17 16:26:02 +0530

    fix case issue

--- PostGram/src/main.jsx
+++ PostGram/src/main.jsx
@@ -8,7 +8,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
 
-import Addpost from "./pages/AddPost";
+import Addpost from "./pages/Addpost";
 
 import EditPost from "./pages/EditPost";
 
