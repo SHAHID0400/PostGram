1b30e35c5c692321774354839525a37dce4e3783
MD SHAHID <shahid.pp23001@gmail.com>
2026-05-18 11:24:26 +0530

    content display

--- PostGram/src/components/PostCard.jsx
+++ PostGram/src/components/PostCard.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import appwriteServices from "../Appwrite/config";
 import { Link } from "react-router-dom";
 const PostCard = ({ post }) => {
-
-    console.log(
-  "IMAGE URL:",
-  appwriteServices.getFileView(post.featuredImage)
-  
-);
-console.log("IMAGE URL TYPE:", typeof appwriteServices.getFileView(post.featuredImage));
+  console.log("IMAGE URL:", appwriteServices.getFileView(post.featuredImage));
+  console.log(
+    "IMAGE URL TYPE:",
+    typeof appwriteServices.getFileView(post.featuredImage),
+  );
   return (
     <Link to={`/post/${post.$id}`}>
       <div className="w-full rounded-xl bg-gray-100 p-4 cursor-pointer sm:p-5 hover:scale-105 transition">
@@ -22,9 +20,13 @@ console.log("IMAGE URL TYPE:", typeof appwriteServices.getFileView(post.featured
             alt={post.title}
             className="rounded-xl w-full h-48 object-cover sm:h-48"
           />
-        </div>  
+        </div>
 
         <h2 className="text-xl font-bold text-black">{post.title}</h2>
+        <div
+          className="text-gray-700 mt-2"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
       </div>
     </Link>
   );
